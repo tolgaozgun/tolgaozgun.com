6c066d8d9990f6e7c79284aea0838d810ef15d6e
Tolga Özgün <tolgaozgunn@gmail.com>
2024-02-19 22:37:11 +0300

    Add resume links

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,8 +5,8 @@ export const GITHUB_URL = 'https://www.github.com/tolgaozgun';
 export const YOUTUBE_URL = 'https://www.youtube.com/@tolgaozgun';
 export const EMAIL_URL = 'mailto:tolgaozgunn@gmail.com';
 export const ACADEMIC_RESUME_URL =
-  'https://drive.google.com/file/d/1XO5Xl2h9J8z3Ft3zX5L8zjG3VvZa4L3P/view?usp=sharing';
+  'https://drive.google.com/file/d/1TYYSKt-2f256kTMexlC06p28w02RfMKa/view';
 export const PROFESSIONAL_RESUME_URL =
-  'https://drive.google.com/file/d/1XO5Xl2h9J8z3Ft3zX5L8zjG3VvZa4L3P/view?usp=sharing';
+  'https://drive.google.com/file/d/1gmIS5Q3s9OVemMDhkJ4XxqIbJtV2c9WE/view';
 export const AVATAR_URL =
   'https://media.licdn.com/dms/image/D4D03AQGw_v52AoVu-A/profile-displayphoto-shrink_800_800/0/1686460768504?e=1713398400&v=beta&t=fipf7YSnAmD3yh6Ppy0iG4ESI8gXgjSMvsWTTk5obuU';
